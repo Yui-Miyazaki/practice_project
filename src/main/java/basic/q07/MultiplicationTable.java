72048f90f2cbdc9f50b7e78c6bd8efbf5575e0bd
Miyazaki Yui <y-miyazaki@seassist.co.jp>
2024-09-09 13:02:56 +0900

    fix 1-Java01

--- src/main/java/basic/q07/MultiplicationTable.java
+++ src/main/java/basic/q07/MultiplicationTable.java
@@ -4,11 +4,17 @@ public class MultiplicationTable {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 9; i++) {
 			for (int j = 1; j <= 9; j++) {
-				System.out.print(i * j + " ");
+				int m = i * j;
+
+				System.out.print(" ");
+				if (10 > m)
+					System.out.print(" ");
+
+				System.out.print(m);
 			}
+
 			System.out.println();
 		}
-
 	}
 
 }
